0a55cd3b28104571af5cb0a0dc1afb0c8e9c85c5
stefano <stefano@zebedee.io>
2025-06-13 10:07:25 -0400

    adding tools

--- agents.py
+++ agents.py
@@ -3,6 +3,35 @@ from textwrap import dedent
 from langchain.llms import OpenAI, Ollama
 from langchain_openai import ChatOpenAI
 
+"""
+Creating Agents Cheat Sheet:
+- Think like a boss.  Work backwards from the goal and think which employee you need to hire
+  to get the job done.
+- Define the Captain of the crew who orient the other agents towards the goal.
+- Define which experts the captain need to communciate with and delegate tasks to.
+  Build a top down structure of the crew.
+
+
+Goal:
+- Create a 7-day travel itinerary with detailed per-day plans,
+  including budget, packaging suggestions and safety tips.
+
+Captain/Manager/Boss:
+- Expert Travel Agent
+
+Employees/Experts to hire:
+- City Selection Expert
+- Local Tour Guide
+
+
+Notes:
+- Agents should be rsults driven and have a clear goal in mind.
+- Role is their job title.
+- Goals should be actionable
+- Backstory should be their resume.
+
+"""
+
 
 # This is an example of how to define custom agents.
 # You can define as many agents as you want.
@@ -13,22 +42,48 @@ class CustomAgents:
         self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
         self.Ollama = Ollama(model="openhermes")
 
-    def agent_1_name(self):
+    def expert_travel_agent(self):
         return Agent(
-            role="Define agent 1 role here",
-            backstory=dedent(f"""Define agent 1 backstory here"""),
-            goal=dedent(f"""Define agent 1 goal here"""),
+            # Role is their job title
+            role="""Expert Travel Agent""",
+            # Backstory is their resume
+            backstory=dedent(
+                """Expert in travel planning and logistics.  I have decades making
+                travel itineraries for clients."""
+            ),
+            goal=dedent(
+                """Create a 7-day travel itinerary with detailed per-day plans,
+                including budget, packing suggestions and safety tips."""
+            ),
+            # tools=[tool_1, tool_2],
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
+
+    def city_selection_expert(self):
+        return Agent(
+            role="""City Selection Expert""",
+            backstory=dedent(
+                """Expert at analyzing travel data to pick ideal destinations."""
+            ),
+            goal=dedent(
+                """Select the best cities based on weather, season, prices and traveler
+                interests."""
+            ),
             # tools=[tool_1, tool_2],
             allow_delegation=False,
             verbose=True,
             llm=self.OpenAIGPT35,
         )
 
-    def agent_2_name(self):
+    def local_tour_guide(self):
         return Agent(
-            role="Define agent 2 role here",
-            backstory=dedent(f"""Define agent 2 backstory here"""),
-            goal=dedent(f"""Define agent 2 goal here"""),
+            role="""Local Toour Guide""",
+            backstory=dedent(
+                """Knowledgeable local guide with extensive information about the city,
+                it's attractions and customs."""
+            ),
+            goal=dedent("""Provide the BEST insights about the selected city."""),
             # tools=[tool_1, tool_2],
             allow_delegation=False,
             verbose=True,
--- tasks.py
+++ tasks.py
@@ -2,6 +2,31 @@
 from crewai import Task
 from textwrap import dedent
 
+"""
+Creating Tasks Cheat Sheet:
+- Begin with the end in mind.  Identify the specific outcome your tasks are aiming
+  to achieve.
+- Break down the outcome into actional tasks, assigning each task to the
+  appropriate agent.
+- Ensure tasks are descriptive, providing clear instructiosn and expected deliverables.
+
+Goal:
+- Develop a detailed iteinerary, including city selelction, attractions
+  and practical travel advice.
+
+Key Steps for Task Creation:
+1. Identify the Desired Outcome: Define what success looks like for your project.
+
+2. Task Breakdown: Divide the goals into smaller, manageable tasks that agents can execute.
+
+3. Assign Tasks to Agents: Match tasks with agents based on their roles and expertise.
+
+4. Task Description Template:
+- Use this template as a guide to define each task in your CrewAI application.
+- This template helps ensure that each task is clearly defined, actionable and aligned with the specific.
+
+"""
+
 
 class CustomTasks:
     def __tip_section(self):
@@ -12,11 +37,11 @@ class CustomTasks:
             description=dedent(
                 f"""
             Do something as part of task 1
-            
+
             {self.__tip_section()}
-    
+
             Make sure to use the most recent data as possible.
-    
+
             Use this variable: {var1}
             And also this variable: {var2}
         """
@@ -30,7 +55,7 @@ class CustomTasks:
             description=dedent(
                 f"""
             Take the input from task 1 and do something with it.
-                                       
+
             {self.__tip_section()}
 
             Make sure to do something else.
